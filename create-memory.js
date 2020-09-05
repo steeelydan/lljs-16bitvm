51ad5d105442b7c6495ce38cdd187dcd0e9bc64f
Steeely Dan <steeelydan@posteo.de>
2020-09-05 15:20:52 +0200

    Initial commit

---
+++ create-memory.js
@@ -0,0 +1,8 @@
+const createMemory = (sizeInBytes) => {
+    const arrayBuffer = new ArrayBuffer(sizeInBytes);
+    const dataView = new DataView(arrayBuffer);
+
+    return dataView;
+};
+
+module.exports = createMemory;
